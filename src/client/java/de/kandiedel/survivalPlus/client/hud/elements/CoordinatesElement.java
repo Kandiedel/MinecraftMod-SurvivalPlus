22b4eca9d91c19012f3341c10b03a98794506d94
Kandiedel <triebelfinn@gmail.com>
2025-12-22 19:17:27 +0100

    noTicket: Fix coords wrong displayed bug

--- src/client/java/de/kandiedel/survivalPlus/client/hud/elements/CoordinatesElement.java
+++ src/client/java/de/kandiedel/survivalPlus/client/hud/elements/CoordinatesElement.java
@@ -15,8 +15,8 @@ public class CoordinatesElement extends HudElement {
     public void render(DrawContext context, MinecraftClient client) {
         if (client.player == null) return;
 
-        context.drawText(client.textRenderer, String.format("§c§lX: §7%.0f", client.player.getX()), MARGIN, MARGIN, COLOR_WHITE, true);
-        context.drawText(client.textRenderer, String.format("§a§lY: §7%.0f", client.player.getY()), MARGIN, MARGIN + LINE_HEIGHT, COLOR_WHITE, true);
-        context.drawText(client.textRenderer, String.format("§9§lZ: §7%.0f", client.player.getZ()), MARGIN, MARGIN + LINE_HEIGHT * 2, COLOR_WHITE, true);
+        context.drawText(client.textRenderer, String.format("§c§lX: §7%d", client.player.getBlockPos().getX()), MARGIN, MARGIN, COLOR_WHITE, true);
+        context.drawText(client.textRenderer, String.format("§a§lY: §7%d", client.player.getBlockPos().getY()), MARGIN, MARGIN + LINE_HEIGHT, COLOR_WHITE, true);
+        context.drawText(client.textRenderer, String.format("§9§lZ: §7%d", client.player.getBlockPos().getZ()), MARGIN, MARGIN + LINE_HEIGHT * 2, COLOR_WHITE, true);
     }
 }
