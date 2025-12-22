7b0d7d822ea69daba92342e85db9800b0c6dcef8
Kandiedel <triebelfinn@gmail.com>
2025-12-22 14:53:48 +0100

    MinecraftMod-SurvivalPlus

---
+++ src/client/java/de/kandiedel/survivalPlus/client/SurvivalPlusClient.java
@@ -0,0 +1,10 @@
+package de.kandiedel.survivalPlus.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SurvivalPlusClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/de/kandiedel/survivalPlus/SurvivalPlus.java
@@ -0,0 +1,10 @@
+package de.kandiedel.survivalPlus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SurvivalPlus implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
