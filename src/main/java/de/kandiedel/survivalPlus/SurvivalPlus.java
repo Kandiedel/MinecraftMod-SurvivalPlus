7b44c15065c248603295151bb411a205fb6013ed
Kandiedel <triebelfinn@gmail.com>
2025-12-22 15:43:53 +0100

    noTicket: Created initialize message

--- src/main/java/de/kandiedel/survivalPlus/SurvivalPlus.java
+++ src/main/java/de/kandiedel/survivalPlus/SurvivalPlus.java
@@ -6,5 +6,18 @@ public class SurvivalPlus implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        System.out.println("");
+        System.out.println("------------------------------------------------");
+        System.out.println("");
+        System.out.println(" [ ✔ ] "
+                + "SurvivalPlus"
+                + " successfully enabled!"
+                + " [ ✔ ]");
+        System.out.println("         "
+                + "Developed By "
+                + "Kandiedel ⚒ ");
+        System.out.println("");
+        System.out.println("------------------------------------------------");
+        System.out.println("");
     }
 }
